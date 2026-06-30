7fad9c700fa095552bff8b35a33f9bcf5e7de52c
Niklas Johansson <raphexion@gmail.com>
2026-06-30 21:28:45 +0200

    Initial commit

---
+++ lib/hawk.ex
@@ -0,0 +1,18 @@
+defmodule Hawk do
+  @moduledoc """
+  Documentation for `Hawk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hawk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
