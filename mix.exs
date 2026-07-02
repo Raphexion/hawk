03171a69affd7d232bb8e55b46755475adcbccb0
Niklas Johansson <raphexion@gmail.com>
2026-07-02 22:18:41 +0200

    Add credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Hawk.MixProject do
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -25,10 +26,23 @@ defmodule Hawk.MixProject do
       # applications own their concrete Repo modules and database config.
       {:ecto, "~> 3.14"},
       {:ecto_sql, "~> 3.14"},
-      {:postgrex, "~> 0.22.2"}
+      {:postgrex, "~> 0.22.2"},
+      # Code quality
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/videdal"]
   defp elixirc_paths(_env), do: ["lib"]
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:ex_unit, :mix],
+      ignore_warnings: "dialyzer_ignore.exs",
+      list_unused_filters: true,
+      plt_local_path: "priv/plts/project.plt",
+      plt_core_path: "priv/plts/core.plt"
+    ]
+  end
 end
