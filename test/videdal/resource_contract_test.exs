34675e04e04bade40f03ea36a75741e39b835b9d
Niklas Johansson <raphexion@gmail.com>
2026-07-05 15:05:10 +0200

    Add high level test of all videdal resources

---
+++ test/videdal/resource_contract_test.exs
@@ -0,0 +1,47 @@
+defmodule Videdal.CourseGradeSummariesContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.CourseGradeSummaries,
+    model: Videdal.CourseGradeSummary
+end
+
+defmodule Videdal.CoursesContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Courses,
+    model: Videdal.Course
+end
+
+defmodule Videdal.EnrollmentsContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Enrollments,
+    model: Videdal.Enrollment
+end
+
+defmodule Videdal.GradesContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Grades,
+    model: Videdal.Grade
+end
+
+defmodule Videdal.ParentsContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Parents,
+    model: Videdal.Parent
+end
+
+defmodule Videdal.SchoolsContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Schools,
+    model: Videdal.School
+end
+
+defmodule Videdal.StudentsContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Students,
+    model: Videdal.Student
+end
+
+defmodule Videdal.TeachersContractTest do
+  use Hawk.ResourceContractCase,
+    resource: Videdal.Teachers,
+    model: Videdal.Teacher
+end
