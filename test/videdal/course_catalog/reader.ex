5f8d695d4a5a8163d758a936e739320e62956232
Niklas Johansson <raphexion@gmail.com>
2026-07-22 10:59:41 +0200

    Fix broken one bang in tests

--- test/videdal/course_catalog/reader.ex
+++ test/videdal/course_catalog/reader.ex
@@ -1,7 +1,15 @@
 defmodule Videdal.CourseCatalog.Reader do
   @moduledoc false
 
-  def one(_opts), do: :not_found
-  def one!(_opts), do: raise("not used")
+  def one(%{missing?: true}), do: :not_found
+  def one(_opts), do: {:ok, %Videdal.Course{}}
+
+  def one!(opts) do
+    case one(opts) do
+      {:ok, course} -> course
+      :not_found -> raise "expected one result, got none"
+    end
+  end
+
   def all(_opts), do: []
 end
--- test/videdal/internal_notes/reader.ex
+++ test/videdal/internal_notes/reader.ex
@@ -1,7 +1,15 @@
 defmodule Videdal.InternalNotes.Reader do
   @moduledoc false
 
-  def one(_opts), do: :not_found
-  def one!(_opts), do: raise("not used")
+  def one(%{missing?: true}), do: :not_found
+  def one(_opts), do: {:ok, %Videdal.InternalNote{}}
+
+  def one!(opts) do
+    case one(opts) do
+      {:ok, note} -> note
+      :not_found -> raise "expected one result, got none"
+    end
+  end
+
   def all(_opts), do: []
 end
